f7866a73d910172790030d9ffd43540d65d833aa
shadowwalker005 <66953516+shadowwalker005@users.noreply.github.com>
2020-06-15 17:37:20 +0500

    Add files via upload

---
+++ shadow.sh
@@ -0,0 +1,16 @@
+#!/system/bin/sh
+
+cd
+
+pkg update
+
+curl -LO https://github.com/termux/termux-packages/files/3995119/metasploit_5.0.65-1_all.deb.gz
+
+
+gunzip metasploit_5.0.65-1_all.deb.gz
+
+
+dpkg -i metasploit_5.0.65-1_all.deb
+
+
+apt -f install
